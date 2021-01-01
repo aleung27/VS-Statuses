8900f4fdcd6565826607de1c1acb385108eee2c1
adam <adamjleung123@gmail.com>
2021-01-01 15:38:48 +1100

    Moved authentication to extension with POST to API

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,16 @@
-import { commands, ExtensionContext, window } from "vscode";
+import { commands, ExtensionContext, window, authentication } from "vscode";
 import ActiveUser from "./ActiveUser";
+import axios from "axios";
+import https from "https";
 
 const user = new ActiveUser();
+
+// Remove this later, for local dev
+const api = axios.create({
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
+});
 let interval: NodeJS.Timeout | null = null;
 
 export function activate(context: ExtensionContext) {
@@ -23,14 +32,36 @@ export function activate(context: ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = commands.registerCommand("vstatus.helloWorld", () => {
+  let disposable = commands.registerCommand("vstatus.helloWorld", async () => {
     // The code you place here will be executed every time your command is executed
 
     // Display a message box to the user
     window.showInformationMessage("Hello VS Code!");
   });
 
-  context.subscriptions.push(update, disposable, stopUpdate);
+  let authenticate = commands.registerCommand("vstatus.auth", async () => {
+    try {
+      const session = await authentication.getSession("github", ["read:user"], {
+        createIfNone: true,
+        clearSessionPreference: false,
+      });
+      console.log("Authenticated, posting to api");
+      const res = (
+        await api.post("https://localhost:8000/auth", {
+          accessToken: session.accessToken,
+        })
+      ).data;
+      console.log(res.accessToken);
+      console.log(res.refreshToken);
+    } catch (err) {
+      console.log(err);
+      window.showInformationMessage(
+        "Error occured during authentication. Please try again"
+      );
+    }
+  });
+
+  context.subscriptions.push(update, disposable, stopUpdate, authenticate);
 }
 
 // this method is called when your extension is deactivated
