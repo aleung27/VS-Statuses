125a0ed7eb8de5ef078b518762ffc053a40cfd4d
adam <adamjleung123@gmail.com>
2021-01-26 12:32:11 +1100

    Fine-grained toggling of settings with scopes

--- src/utilities/settings.ts
+++ src/utilities/settings.ts
@@ -1,4 +1,4 @@
-import { workspace } from "vscode";
+import { ConfigurationTarget, workspace } from "vscode";
 
 /**
  * Enum for the possible setting options
@@ -10,22 +10,36 @@ enum Setting {
 }
 
 /**
- * Toggles a setting in the **global** scope for the vscode extension
+ * Toggles a setting in the highest changed scope for the vscode extension.
+ * This is language choice independent and will not alter the settings for
+ * specific language ids.
+ * Heirarchy: WorkspaceFolder > Workspace > Global
  * @param choice The setting to toggle as defined in the enum
  * @param toOn Whether we are toggling to On or not
  */
-//TODO: precedence and heirarchy for workspaceFodler > workspace > global
 const toggleSetting = async (choice: string, toOn: boolean) => {
   const configOptions = workspace.getConfiguration("vs-statuses");
   const currVal = configOptions.get<boolean>(choice);
+  const configList = configOptions.inspect<boolean>(choice);
+  let target: ConfigurationTarget;
 
   // Check that it actually needs toggling
-  if (currVal === toOn) {
+  if (configList === undefined || currVal === undefined || currVal === toOn) {
     return;
   }
 
+  // Decide the target we need to change. The highest priority
+  // target that has been modified before is the one that is changed
+  if (configList.workspaceFolderValue === !toOn) {
+    target = ConfigurationTarget.WorkspaceFolder;
+  } else if (configList.workspaceValue === !toOn) {
+    target = ConfigurationTarget.Workspace;
+  } else {
+    target = ConfigurationTarget.Global;
+  }
+
   try {
-    await configOptions.update(choice, toOn, true);
+    await configOptions.update(choice, toOn, target);
   } catch (err) {
     console.log(err);
   }
